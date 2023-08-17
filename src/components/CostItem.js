fd4c09fd0298efd66b3ae38be6e147ec056342df
Dmitry <dmitry27zh@gmail.com>
2023-08-17 16:11:32 +0300

    Format date in CostItem

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -2,10 +2,21 @@ import './CostItem.css'
 
 function CostItem(props) {
   const { date, title, price } = props
+  const month = date.toLocaleString('en', {
+    month: 'long',
+  })
+  const year = date.getFullYear()
+  const day = date.toLocaleString('en', {
+    day: '2-digit',
+  })
 
   return (
     <div className="cost-item">
-      <div>{date.toLocaleDateString('ru')}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="cost-item__description">
         <h2>{title}</h2>
         <div className="cost-item__price">${price}</div>
