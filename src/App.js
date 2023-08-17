bba868836c8140e32630d5c5ce0471a6fe57926d
Dmitry <dmitry27zh@gmail.com>
2023-08-17 16:23:59 +0300

    Refactor App

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ function App() {
   return (
     <div>
       <h1>Start learning React!</h1>
-      <CostItem date={costs[0].date} title={costs[0].title} price={costs[0].price}></CostItem>
-      <CostItem date={costs[1].date} title={costs[1].title} price={costs[1].price}></CostItem>
-      <CostItem date={costs[2].date} title={costs[2].title} price={costs[2].price}></CostItem>
+      <CostItem date={costs[0].date} title={costs[0].title} price={costs[0].price} />
+      <CostItem date={costs[1].date} title={costs[1].title} price={costs[1].price} />
+      <CostItem date={costs[2].date} title={costs[2].title} price={costs[2].price} />
     </div>
   )
 }
