264fd8383384b56b4f14744b825764e0a261963d
Dmitry <dmitry27zh@gmail.com>
2023-08-17 16:28:12 +0300

    Split CostItem

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,22 +1,12 @@
+import CostDate from './CostDate'
 import './CostItem.css'
 
 function CostItem(props) {
   const { date, title, price } = props
-  const month = date.toLocaleString('en', {
-    month: 'long',
-  })
-  const year = date.getFullYear()
-  const day = date.toLocaleString('en', {
-    day: '2-digit',
-  })
 
   return (
     <div className="cost-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <CostDate date={date} />
       <div className="cost-item__description">
         <h2>{title}</h2>
         <div className="cost-item__price">${price}</div>
