f78dfec53a548059ca399cf2e8fde24db11b9eca
Dmitry <dmitry27zh@gmail.com>
2023-08-19 19:08:56 +0300

    Add Card wrapper component

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,17 +1,18 @@
 import CostDate from './CostDate'
 import './CostItem.css'
+import Card from './Card'
 
 function CostItem(props) {
   const { date, title, price } = props
 
   return (
-    <div className="cost-item">
+    <Card className="cost-item">
       <CostDate date={date} />
       <div className="cost-item__description">
         <h2>{title}</h2>
         <div className="cost-item__price">${price}</div>
       </div>
-    </div>
+    </Card>
   )
 }
 
--- src/components/Costs.js
+++ src/components/Costs.js
@@ -1,15 +1,16 @@
 import CostItem from './CostItem'
 import './Costs.css'
+import Card from './Card'
 
 function Costs(props) {
   const { costs } = props
 
   return (
-    <div className="costs">
+    <Card className="costs">
       <CostItem date={costs[0].date} title={costs[0].title} price={costs[0].price} />
       <CostItem date={costs[1].date} title={costs[1].title} price={costs[1].price} />
       <CostItem date={costs[2].date} title={costs[2].title} price={costs[2].price} />
-    </div>
+    </Card>
   )
 }
 
