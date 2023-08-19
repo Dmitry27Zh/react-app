5ca8ada4ec9a269e9ff5eaa6f2a092e2ed340274
Dmitry <dmitry27zh@gmail.com>
2023-08-19 18:03:06 +0300

    Add Costs styles, move costs data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import './App.css'
 import Costs from './components/Costs'
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2021, 12, 2),
+      title: 'Fridge',
+      price: '999.99',
+    },
+    {
+      date: new Date(2022, 2, 1),
+      title: 'MacBook',
+      price: '1200',
+    },
+    {
+      date: new Date(2022, 5, 6),
+      title: 'Jeans',
+      price: '20',
+    },
+  ]
+
   return (
     <div>
       <h1>Start learning React!</h1>
-      <Costs />
+      <Costs costs={costs} />
     </div>
   )
 }
--- src/components/Costs.js
+++ src/components/Costs.js
@@ -1,26 +1,11 @@
 import CostItem from './CostItem'
+import './Costs.css'
 
-function Costs() {
-  const costs = [
-    {
-      date: new Date(2021, 12, 2),
-      title: 'Fridge',
-      price: '999.99',
-    },
-    {
-      date: new Date(2022, 2, 1),
-      title: 'MacBook',
-      price: '1200',
-    },
-    {
-      date: new Date(2022, 5, 6),
-      title: 'Jeans',
-      price: '20',
-    },
-  ]
+function Costs(props) {
+  const { costs } = props
 
   return (
-    <div>
+    <div className="costs">
       <CostItem date={costs[0].date} title={costs[0].title} price={costs[0].price} />
       <CostItem date={costs[1].date} title={costs[1].title} price={costs[1].price} />
       <CostItem date={costs[2].date} title={costs[2].title} price={costs[2].price} />
