66ed024acc11905cf36100039c6007b65e14e18b
Dmitry <dmitry27zh@gmail.com>
2023-08-22 17:51:01 +0300

    Unuse state in CostItem

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,17 +1,9 @@
 import CostDate from './CostDate'
 import './CostItem.css'
 import Card from '../UI/Card'
-import { useState } from 'react'
 
 const CostItem = (props) => {
-  const [{ date, name, price }, setCost] = useState(props)
-  const changeDescriptionHandler = () => {
-    setCost({
-      date,
-      price,
-      name: 'New title!',
-    })
-  }
+  const { date, name, price } = props
 
   return (
     <Card className="cost-item">
@@ -19,7 +11,6 @@ const CostItem = (props) => {
       <div className="cost-item__description">
         <h2>{name}</h2>
         <div className="cost-item__price">${price}</div>
-        <button onClick={changeDescriptionHandler}>Change description</button>
       </div>
     </Card>
   )
