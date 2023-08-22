59d0ce08782c8e73b5362ff4d5a359af7c84804c
Dmitry <dmitry27zh@gmail.com>
2023-08-22 17:51:29 +0300

    Add costs state in App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 import Costs from './components/Costs/Costs'
 import NewCost from './components/NewCost/NewCost'
 
+const INITIAL_COSTS = [
+  {
+    id: 'cost 1',
+    date: new Date(2021, 12, 2),
+    name: 'Fridge',
+    price: '999.99',
+  },
+  {
+    id: 'cost 2',
+    date: new Date(2022, 2, 1),
+    name: 'MacBook',
+    price: '1200',
+  },
+  {
+    id: 'cost 3',
+    date: new Date(2022, 5, 6),
+    name: 'Jeans',
+    price: '20',
+  },
+]
+
 const App = () => {
-  const costs = [
-    {
-      id: 'cost 1',
-      date: new Date(2021, 12, 2),
-      name: 'Fridge',
-      price: '999.99',
-    },
-    {
-      id: 'cost 2',
-      date: new Date(2022, 2, 1),
-      name: 'MacBook',
-      price: '1200',
-    },
-    {
-      id: 'cost 3',
-      date: new Date(2022, 5, 6),
-      name: 'Jeans',
-      price: '20',
-    },
-  ]
-  const addNewCostHandler = (costData) => {
-    console.log(costData)
+  const [costs, setCosts] = useState(INITIAL_COSTS)
+  const addNewCostHandler = (newCost) => {
+    setCosts((previousState) => {
+      const updatedCosts = [newCost, ...previousState]
+      console.log(updatedCosts)
+      return updatedCosts
+    })
   }
 
   return (
